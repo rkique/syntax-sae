4a0a2e5ec6f56e6f5c83c2b04cd75b7b21991c1e
Eric Xia <ericpingxia@gmail.com>
2024-11-12 12:49:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ import networkx as nx
 import json
 
 from huggingface_hub import login
-print("logging in")
-login(token="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")
+
 
 tokens = None
 tokenizer = None
